109da1982edce8fbce1a1e6ac8b51f9d2bafca1c
Matt Payne <Payne@MattPayne.org>
2017-07-15 19:51:19 -0500

    Exception does not stop the download

--- msebg.py
+++ msebg.py
@@ -11,25 +11,53 @@
 import requests
 import urllib
 
-download_list = 'http://ligman.me/2sZVmcG'
+def download(book):
+    text = requests.get(book)
 
-res = requests.get (download_list)
-# convert list to array
-books =  (res.text.splitlines())
-# remove header from array
-books.remove ('MSFT eBooks')
-# download books
-for book in books:
-    text = requests.get (book)
-# get real dowwnload name for file
+    # get real dowwnload name for file
     filename = text.url
-# take last part of name / 
+
+    # take last part of name / 
     filename = filename.rsplit('/',1)[1]
-# convert %20 to spaces and other URL values
-    filename = urllib.unquote (filename)
-    print ('Saving: ' + filename)
-# write file out in chunks to system
-    bookfile = open (filename, 'wb')
+
+    # convert %20 to spaces and other URL values
+    filename = urllib.unquote(filename)
+
+    print('Saving: ' + filename)
+
+    # write file out in chunks to system
+    bookfile = open(filename, 'wb')
     for chunk in text.iter_content(100000):
          bookfile.write(chunk)
     bookfile.close()
+
+def main():
+    download_list = 'http://ligman.me/2sZVmcG'
+    res = requests.get(download_list)
+    # convert list to array
+    books =  (res.text.splitlines())
+    # remove header from array
+    books.remove('MSFT eBooks')
+    numBooks = len(books)
+    print("About to download %d books files!" % (numBooks))
+    bookNum = 1
+    exceptionNum = 0
+    # download books
+    for book in books:
+        try:
+           print("Downloading book %d of %d" % (bookNum, numBooks))
+           print("%d exceptions so far." % (exceptionNum))
+           bookNum = bookNum + 1
+           download(book)
+        except TypeError:
+           exceptionNum = exceptionNum + 1
+           print("Skipping book because TypeError")
+        except:
+           exceptionNum = exceptionNum + 1
+           print("Skipping book because exception")
+
+
+if  __name__ =='__main__':
+    main()
+
+
